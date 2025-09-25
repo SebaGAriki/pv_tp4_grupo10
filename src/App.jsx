2b2d20fe7806d7f2148e7ea18650c9a1e52dac84
SebaGAriki <sebastian.g.ariki@gmail.com>
2025-09-25 16:47:05 -0300

    Saludo implementado

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import Saludo from './assets/components/Saludo'
+
+function App() {
+
+  // variables
+  let nombre = 'Sebastian';
+  let apellido= 'Gonzalez Ariki';
+
+  // funciones
+
+
+  return (
+    <>
+      <Saludo nA = {nombre} aA = {apellido} />
+      
+    </>
+    )
+}
+export default App;
---
+++ src/assets/components/Saludo.jsx
@@ -0,0 +1,10 @@
+function Saludo({ nA, aA }) {
+    
+  return (
+    <div>
+      <h2>Este es el componente</h2>
+      <h1>Saludo a {nA} {aA}</h1>
+    </div>
+  )
+}
+export default Saludo;
